01778023087ad26d497b24d7d908956297d7d278
Anand Thakur <anandsingh23112005@gmail.com>
2024-12-18 17:35:47 +0530

    star pattrens practice

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -3,14 +3,79 @@ using namespace std;
 
 
 int main(){
+     //# squre loop-------------
+  // int n = 5;
 
-  int n = 5;
-   for (int i=1;i<n;i++){
-     for(int j =1; j<n;j++){
-      cout <<j;
+  //  for (int i=1;i<n;i++){
+  //    for(int j =1; j<n;j++){
+  //     cout <<j;
+  //    }
+  //    cout<<endl;
+  //  }
+
+ //  #characters square loop-------------
+  //   char ch = 'A';
+  //  for(int i=0 ; i<n; i++){
+    
+  //   for(int j=0; j<n ;j++){
+  //     cout<<ch;
+  //     ch= ch +1;
+   
+  //   };
+
+  //   cout<<endl;
+
+  //  };
+   
+   // ##number square loop ----------
+
+  //     int temp= 1 ;
+
+  // for (int i=1 ; i<n ; i++){
+  
+  //   for (int j = 1; j < n; j++){
+  //     cout<< temp ;
+  //      temp++;
+       
+        
+  //   }
+  //   cout<<endl;
+  // }
+
+
+   // #star triangle loop  -------------------------
+  //   int q=1;
+
+  // for (int i=0; i<n ; i++){
+  //    for(int j=0; j<q ; j++ ){
+  //     cout<< "* "<<"";
+  //    }
+  //     q=q+1;
+  //    cout<<endl;
+  //}
+    // #reapated number triangle loop --------------------
+  //    int p=1;
+  //  for (int i=0; i<n ; i++){
+    
+  //     for(int j=0; j<p ; j++ ){
+  //     cout<< p<<"";
+  //    }
+  //     p=p+1;
+  //    cout<<endl;
+  // }
+
+  // non repeated triangle loop----------------------------
+  int n= 5;
+  int n2=1;
+  for(int i=0;i<=n ; i++){
+     for (int j=0; j<n ; j++){
+         cout<<n2<<" ";
+         n2=n2++;
      }
-     cout<<endl;
-   }
+     
+
+  }
+   
 
  return 0;
  
