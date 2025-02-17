8ed6d51a9494996cb40ed8df60746e955ff799b6
Anand Thakur <anandsingh23112005@gmail.com>
2025-02-17 19:27:37 +0530

    some change

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -79,16 +79,23 @@ int main(){
 
 //---------------------------
 
-int n= 5 ;
+// int n= 5 ;
  
- for (int i=0; i<n; i++ ){
-   for( int j=0 ; j<i ; j++){
+//  for (int i=0; i<n; i++ ){
+//    for( int j=0 ; j<i ; j++){
        
-   };
- }
+//    };
+//  }
 
    
 
- return 0;
+//  return 0;
  
- } ;
+//  } ;
+
+for (int i = 1; i <= 5; ++i) {
+  for (int j = 1; j <= i; ++j) {
+      std::cout << "* ";
+  }
+  std::cout << std::endl;
+}
